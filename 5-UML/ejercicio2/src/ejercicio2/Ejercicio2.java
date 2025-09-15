7995a097fa380838f6374b32ea372ccde9e42026
Alex Austin <austinalex71@gmail.com>
2025-09-15 16:31:08 -0300

    eejrcicio 2 terminado

---
+++ 5-UML/ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,23 @@
+package ejercicio2;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        Bateria bate1 = new Bateria("FR1", "1220mAh");
+        Usuario user1 = new Usuario("Alex Austin", 4255200);
+        Celular celu1 = new Celular("A501", "samsom", "w12", bate1, user1);
+        Bateria bate2 = new Bateria("FQ001", "2250mAh");
+        Usuario user2 = new Usuario("Matias Gonzales", 1200290);
+        Celular celu2 = new Celular("Z01", "Sonic", "Xperia x9", bate1, user1);
+        Bateria bate3 = new Bateria("FR1", "3000mAh");
+        Usuario user3 = new Usuario("Zoe Schumager", 4505299);
+        Celular celu3 = new Celular("A501", "samsom", "QW1", bate1, user1);
+
+        System.out.println(celu1);
+        System.out.println(" ");
+        System.out.println(celu2);
+        System.out.println(" ");
+        System.out.println(celu3);
+    }
+
+}
---
+++ 5-UML/ejercicio2/src/ejercicio2/Usuario.java
@@ -0,0 +1,29 @@
+package ejercicio2;
+
+public class Usuario {
+private String nombre;
+private  int DNI;
+private Celular celular;
+
+    public Usuario(String nombre, int DNI) {
+        this.nombre = nombre;
+        this.DNI = DNI;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", DNI=" + DNI + '}';
+    }
+
+    
+    
+
+
+ 
+
+
+
+
+
+
+}
